4a3e33c78746c47891b408c523e9b31dcfb0f0f5
Miguel Saliba <36637899+miguelsaliba@users.noreply.github.com>
2026-02-24 00:03:37 -0800

    styling fixes

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,26 @@
 <script setup lang="ts">
 import Map from './components/Map.vue';
-import { databaseService } from './services/database';
 import { useLocationStore } from './stores/LocationStore';
 
 const locationStore = useLocationStore();
-
-async function dumpLocation() {
-  if (!locationStore.sessionId) return;
-  console.log(await databaseService.getSessionPoints(locationStore.sessionId));
-}
 </script>
 
 <template>
   <div>
-    <!-- Should use a separate way of getting the location -->
-    <Map :pos="locationStore.lastPosition"/>
-    <div class="bottom-bar">
+    <Map />
+
+    <div class="fixed left-0 right-0 bottom-20 flex justify-center items-center w-full font-semibold size-4 transition-colors ease-in-out">
       <button v-if="!locationStore.isWatching" @click="locationStore.startWatching()"
-        class="rounded-2xl transition-colors ease-in-out font-semibold text-gray-800 backdrop-blur-sm border-2
-        border-gray-800 bg-gray-600/30 dark:border-white dark:text-white hover:backdrop-invert-10">Start</button>
+        class="bg-gray-600/30 text-gray-800 backdrop-blur-sm border-2 border-gray-800 dark:border-white
+        dark:text-white hover:backdrop-invert-10 rounded-2xl">
+        Start
+      </button>
+
       <button v-else @click="locationStore.stopWatching()"
-        class="rounded-2xl transition-colors ease-in-out font-semibold text-gray-800 backdrop-blur-sm border-2
-        border-gray-800 bg-red-600/30 dark:border-white dark:text-white
-        hover:backdrop-invert-10">Stop {{ locationStore.sessionLocations.length }}</button>
-      <button @click="dumpLocation">debug</button>
+        class="bg-red-600/30 text-gray-800 backdrop-blur-sm border-2 border-gray-800 dark:border-white
+        dark:text-white hover:backdrop-invert-10 rounded-2xl">
+        Stop
+      </button>
     </div>
   </div>
 </template>
@@ -32,24 +29,6 @@ async function dumpLocation() {
 button {
   outline: none;
   padding: 1rem 2rem;
-  margin: auto;
   letter-spacing: 0.05rem;
-}
-
-.bottom-bar {
-  position: fixed;
-  left: 0;
-  right: 0;
-  bottom: 2rem;
-  display: flex;
-  margin: auto;
-  font-size: 17px;
-}
-
-.current-location {
-  position: fixed;
-  left: 0;
-  bottom: 0;
-  background-color: grey;
 }
 </style>
